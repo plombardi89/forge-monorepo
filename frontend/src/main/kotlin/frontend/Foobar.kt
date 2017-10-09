027956a95e2378944e4a503c9666aedb44e5de9c
Philip Lombardi <plombardi@datawire.io>
2017-10-09 14:28:51 -0400

    Multiple modules ("monorepo"-esque) Forge + Gradle + JDK (kotlin)

--- frontend/src/main/kotlin/frontend/Foobar.kt
+++ frontend/src/main/kotlin/frontend/Foobar.kt
@@ -2,5 +2,5 @@ package frontend
 
 class Foobar {
 
-  fun foobar() = "I am Foobar: I demonstrate incremental compilation... change me but do not change my call site."
+  fun foobar() = "I am Foobar: I demonstrate incremental compilation..."
 }
